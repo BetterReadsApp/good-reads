d3ccb7ea866f251b063d3cc706bc85ef2606f9f2
MarcoNatalini <mnatalini@fi.uba.ar>
2024-11-05 19:45:12 -0300

    creada la pantalla de agregar libro a estantes

--- app/src/main/kotlin/uba/fi/goodreads/presentation/shelves/addBookScreen/composables/addBookToShelvesScreen.kt
+++ app/src/main/kotlin/uba/fi/goodreads/presentation/shelves/addBookScreen/composables/addBookToShelvesScreen.kt
@@ -1,7 +1,10 @@
 package uba.fi.goodreads.presentation.shelves.addBookScreen.composables
 
-import uba.fi.goodreads.presentation.shelves.shelfBooksScreen.ShelfBooksUiState
-import uba.fi.goodreads.presentation.shelves.shelfBooksScreen.ShelfBooksViewModel
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,33 +12,42 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import uba.fi.goodreads.core.design_system.theme.GoodReadsTheme
 import uba.fi.goodreads.domain.model.Book
+import uba.fi.goodreads.domain.model.Shelf
 import uba.fi.goodreads.presentation.shelves.addBookScreen.AddBookToShelvesParameterProvider
 import uba.fi.goodreads.presentation.shelves.addBookScreen.AddBookToShelvesUiState
 
 
 @Composable
 private fun AddToShelvesScreen(
-    screenState: AddBookToShelvesUiState
+    screenState: AddBookToShelvesUiState,
+    onConfirm: () -> Unit,
+    selectedShelves: MutableSet<String>,
 ) {
 
     val scrollState = rememberScrollState()
@@ -47,9 +59,68 @@ private fun AddToShelvesScreen(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         BookSummary(screenState.book)
-        Spacer(Modifier.weight(1f))
+        Text(
+            text = "SELECT A SHELF",
+            style = MaterialTheme.typography.headlineLarge,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(5.dp))
+        screenState.shelves.forEach{ shelf ->
+            ShelfButton(shelf, isSelected = shelf.name in selectedShelves)
+            { isSelected ->
+                if (isSelected) {
+                    selectedShelves.add(shelf.name)
+                } else {
+                    selectedShelves.remove(shelf.name)
+                }
+            }
+            }
+        Spacer(modifier = Modifier.height(16.dp))
 
+        ConfirmButton(onClick = onConfirm)
+        }
+
+
+}
+
+
+
+@Composable
+fun ShelfButton(shelf: Shelf,
+                isSelected: Boolean,
+                onSelectionChange: (Boolean) -> Unit){
+
+    var selected: Boolean by remember { mutableStateOf(isSelected) }
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+
+        Text(
+            text = shelf.name,
+            fontSize = 18.sp,
+            modifier = Modifier.weight(1f)
+        )
+
+        Text(
+            text = shelf.numberOfBooks.toString() + "libros",
+            fontSize = 16.sp,
+            modifier = Modifier.padding(end = 16.dp)
+        )
+
+        Checkbox(
+            checked = selected,
+            onCheckedChange = {
+                selected = it
+                onSelectionChange(it)
+            }
+        )
     }
+
 }
 
 @Composable
@@ -88,6 +159,24 @@ fun BookSummary(book: Book) {
     }
 }
 
+@Composable
+fun ConfirmButton(onClick: () -> Unit) {
+    Box (
+        modifier = Modifier
+            .width(200.dp)
+            .height(50.dp)
+            .background(Color.LightGray)
+            .clickable(onClick = onClick),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Confirm Selection",
+            style = MaterialTheme.typography.bodyMedium,
+            color = Color.Black
+        )
+    }
+}
+
 
 
 @Composable
@@ -96,7 +185,10 @@ fun AddToShelvesScreenPreview(
     @PreviewParameter(AddBookToShelvesParameterProvider::class) state: AddBookToShelvesUiState
 ) {
     GoodReadsTheme {
-        AddToShelvesScreen(screenState = state
+        AddToShelvesScreen(
+            screenState = state,
+            selectedShelves = mutableSetOf(),
+            onConfirm = {}
         )
     }
 }
