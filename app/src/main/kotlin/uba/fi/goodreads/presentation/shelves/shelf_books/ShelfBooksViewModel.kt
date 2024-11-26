f817d48d7af55d530852fedf8d72f5e164245c24
MarcoNatalini <mnatalini@fi.uba.ar>
2024-11-26 10:51:50 -0300

    terminado renombrar shelf

--- app/src/main/kotlin/uba/fi/goodreads/presentation/shelves/shelf_books/ShelfBooksViewModel.kt
+++ app/src/main/kotlin/uba/fi/goodreads/presentation/shelves/shelf_books/ShelfBooksViewModel.kt
@@ -10,13 +10,17 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import uba.fi.goodreads.domain.usecase.GetShelfBooksUseCase
+import uba.fi.goodreads.domain.usecase.RenameShelfUseCase
+import uba.fi.goodreads.domain.usecase.DeleteShelfUseCase
 import uba.fi.goodreads.presentation.shelves.shelf_books.navigation.ShelfBooksDestination
 import javax.inject.Inject
 
 @HiltViewModel
 class ShelfBooksViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
-    private val getShelfBooksUseCase: GetShelfBooksUseCase
+    private val getShelfBooksUseCase: GetShelfBooksUseCase,
+    private val deleteShelfUseCase: DeleteShelfUseCase,
+    private val renameShelfUseCase: RenameShelfUseCase
 ): ViewModel() {
 
     private val _screenState: MutableStateFlow<ShelfBooksUiState> =
@@ -52,4 +56,40 @@ class ShelfBooksViewModel @Inject constructor(
         _screenState.update { it.copy(destination = null) }
     }
 
+    fun onRenameShelf(newName: String) {
+        viewModelScope.launch{
+            renameShelfUseCase(shelfId = shelfId , name = newName).also { result ->
+                when (result) {
+                    is RenameShelfUseCase.Result.Error,
+                    is RenameShelfUseCase.Result.UnexpectedError -> Unit
+
+                    is RenameShelfUseCase.Result.Success -> _screenState.update {
+                        ShelfBooksUiState(
+                            name = result.shelf.name,
+                            id = result.shelf.id,
+                            books = result.shelf.books
+                        )
+                    }
+                }
+            }
+        }
+
+    }
+
+
+    fun onDeleteShelf() {
+        viewModelScope.launch{
+            deleteShelfUseCase(shelfId = shelfId).also { result ->
+                when (result) {
+                    is DeleteShelfUseCase.Result.Error,
+                    is DeleteShelfUseCase.Result.UnexpectedError -> Unit
+                    is DeleteShelfUseCase.Result.Success -> _screenState.update {
+                        it.copy(destination = ShelfBooksDestination.Back)
+                    }
+                }
+            }
+        }
+    }
 }
+
+
--- app/src/main/kotlin/uba/fi/goodreads/presentation/shelves/shelf_books/composables/ShelfBooksScreen.kt
+++ app/src/main/kotlin/uba/fi/goodreads/presentation/shelves/shelf_books/composables/ShelfBooksScreen.kt
@@ -1,27 +1,43 @@
 package uba.fi.goodreads.presentation.shelves.shelf_books.composables
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import uba.fi.goodreads.domain.model.Book
 import uba.fi.goodreads.presentation.shelves.shelf_books.ShelfBooksUiState
 import uba.fi.goodreads.presentation.shelves.shelf_books.ShelfBooksViewModel
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -36,7 +52,6 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import uba.fi.goodreads.core.design_system.theme.GoodReadsTheme
-import uba.fi.goodreads.presentation.book_info.composables.RatingStars
 import uba.fi.goodreads.presentation.shelves.shelf_books.ShelfBooksPreviewParameterProvider
 import uba.fi.goodreads.presentation.shelves.shelf_books.navigation.ShelfBooksDestination
 
@@ -56,37 +71,160 @@ fun ShelfBooksRoute(
 
     ShelfBooksScreen(
         screenState = screenState,
-        onBookClick = viewModel::onBookClick
+        onBookClick = viewModel::onBookClick,
+        onRenameShelf = viewModel::onRenameShelf,
+        onDeleteShelf = viewModel::onDeleteShelf
     )
 }
 
 @Composable
 private fun ShelfBooksScreen(
     screenState: ShelfBooksUiState,
-    onBookClick: (String) -> Unit
+    onBookClick: (String) -> Unit,
+    onRenameShelf: (String) -> Unit,
+    onDeleteShelf: () -> Unit
 ) {
-
     val scrollState = rememberScrollState()
+    var showMenu by remember { mutableStateOf(false) }
+    var showRenameDialog by remember { mutableStateOf(false) }
+    var showDeleteDialog by remember { mutableStateOf(false) }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp)
             .verticalScroll(scrollState),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            modifier = Modifier.padding(vertical = 32.dp),
-            text = screenState.name,
-            fontSize = 32.sp
-        )
+    )
+    {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 32.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = screenState.name,
+                fontSize = 32.sp,
+                modifier = Modifier.weight(1f)
+            )
+            Box{
+                IconButton(onClick = { showMenu = true }) {
+                    Icon(imageVector = Icons.Default.MoreVert, contentDescription = "Menu")
+                }
+                DropdownMenu(
+                    expanded = showMenu,
+                    onDismissRequest = { showMenu = false },
+                    modifier = Modifier
+                        .wrapContentWidth(Alignment.End)
+
+                ){
+                    DropdownMenuItem(
+                        onClick = {
+                            showMenu = false
+                            showRenameDialog = true
+                        },
+                        text = { Text("Rename") }
+                    )
+                    DropdownMenuItem(onClick = {
+                        showMenu = false
+                        showDeleteDialog = true
+                    },
+                        text = { Text("Delete") }
+                    )
+                }
+            }
+
+        }
         screenState.books.forEach { book ->
             BookSummary(book, onBookClick = onBookClick)
             Spacer(Modifier.height(16.dp))
         }
-        Spacer(Modifier.weight(1f))
+        Spacer(modifier = Modifier.fillMaxHeight().weight(1f))
+    }
 
+    if (showRenameDialog) {
+        RenameDialog(
+            currentName = screenState.name,
+            onDismiss = { showRenameDialog = false },
+            onConfirm = { newName ->
+                showRenameDialog = false
+                onRenameShelf(newName)
+            }
+        )
+    }
+
+    if (showDeleteDialog) {
+        DeleteDialog(
+            onDismiss = { showDeleteDialog = false },
+            onConfirm = {
+                showDeleteDialog = false
+                onDeleteShelf()
+            }
+        )
     }
 }
+
+
+@Composable
+private fun RenameDialog(
+    currentName: String,
+    onDismiss: () -> Unit,
+    onConfirm: (String) -> Unit
+) {
+    var newName by remember { mutableStateOf(currentName) }
+
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        title = { Text("Rename Shelf") },
+        text = {
+            Column {
+                Text("Enter a new name for the shelf:")
+                Spacer(modifier = Modifier.height(8.dp))
+                TextField(
+                    value = newName,
+                    onValueChange = { newName = it },
+                    placeholder = { Text("New shelf name") }
+                )
+            }
+        },
+        confirmButton = {
+            TextButton(
+                onClick = { onConfirm(newName) }
+            ) {
+                Text("Confirm")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismiss) {
+                Text("Cancel")
+            }
+        }
+    )
+}
+
+@Composable
+private fun DeleteDialog(
+    onDismiss: () -> Unit,
+    onConfirm: () -> Unit
+) {
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        title = { Text("Delete Shelf") },
+        text = { Text("Are you sure you want to delete this shelf?") },
+        confirmButton = {
+            TextButton(onClick = onConfirm) {
+                Text("Confirm")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismiss) {
+                Text("Cancel")
+            }
+        }
+    )
+}
+
 
 @Composable
 fun BookSummary(book: Book, onBookClick: (String) -> Unit ) {
@@ -146,7 +284,9 @@ fun ShelvesScreenPreview(
     GoodReadsTheme {
         ShelfBooksScreen(
             screenState = state,
-            onBookClick = { }
+            onBookClick = { },
+            onRenameShelf = {},
+            onDeleteShelf = {}
         )
     }
 }
